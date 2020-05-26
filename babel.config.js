0332297981b85e814d3cd1f4833dbbe093ed50b8
Andreas Rudenå <andreas@rudena.se>
2020-05-26 15:18:42 +0200

    chore: add module2 to resolver and add babel in root config

--- babel.config.js
+++ babel.config.js
@@ -2,10 +2,21 @@ const path = require('path');
 
 // Todo: read this fs from lerna.json file and use package.json in each module..?
 const packages = {
-  'module1': path.resolve(__dirname, 'packages/module1')
+  'module1': path.resolve(__dirname, 'packages/module1'),
+  'module2': path.resolve(__dirname, 'packages/module2')
 };
 
 const config = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        'targets': {
+          'node': 'current'
+        }
+      }
+    ]
+  ],
   plugins: [
     [
       'module-resolver',
@@ -18,7 +29,6 @@ const config = {
   ],
   ignore: [filename => {
     if (!/\/node_modules\//.test(filename)) {
-      console.log(`File found for compiling: ${filename}`);
       return false;
     } 
     return true;
